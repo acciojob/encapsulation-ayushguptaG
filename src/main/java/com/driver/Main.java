8eb265254d4f9a5aac6dbf504907044bfb86d6ed
Ayush Gupta <ayushgupta068@gmail.com>
2022-12-13 03:21:38 +0530

    Added classes in package

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,13 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String args[]){
+       RWOnly obj= new RWOnly();
+
+//        obj.name= "Ayush";
+//        error- 'name' has private access in 'com.driver.RWOnly'
+
+      obj.setName("Jaddu");
+      System.out.println(obj.getName());
+  }
 }
